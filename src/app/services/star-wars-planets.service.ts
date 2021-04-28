f8b9cb2355923e7c999667a0fe477eddf2a63a91
unknown <you@example.com>
2021-04-28 11:56:44 -0300

    changed from absolute to relative path

--- src/app/services/star-wars-planets.service.ts
+++ src/app/services/star-wars-planets.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 import { PlanetResponse } from '../data/planet-response-interface';
 import { PlanetsRequest } from '../data/planets-request-interface';
 import { GeneralService } from './general.service';
